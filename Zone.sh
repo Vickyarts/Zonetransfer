0c23181f8f65ea8981d7af6ade9c1a0711232b42
Vickyarts <65495793+Vickyarts@users.noreply.github.com>
2020-07-30 05:26:29 +0000

    Update Zone.sh

--- Zone.sh
+++ Zone.sh
@@ -1,22 +1,26 @@
 #!/bin/bash
 #Vickyarts Github
+#Contact me on Github
+red='\e[1;31m'
+yellow='\e[1;33m'
+blue='\e[1;34m'
 
 if [ $# -eq 1 ];
 then
      for names in $(host -t ns $1 | grep "server" | cut -d " " -f 4);
      do
-        echo "Zonetransfer with $names:"
+        echo -e $yellow "Zonetransfer with $names:"
         zone=$(host -l $1 $names | grep "has address")
         if [ "$zone" = "" ];
         then
-          echo "Zone Transfer Failed!"
+          echo -e $red "Zone Transfer Failed!"
         else
-          echo "Zone Transferm Success:"
+          echo -e $blue "Zone Transferm Success:"
           echo "$zone"
         fi
      done
 else
-  echo "Usage:"
+  echo -e $yellow"Usage:"
   echo "Zone.sh <website>"
   exit
 fi
