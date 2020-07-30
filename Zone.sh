59ccc7d687aecaeefdd19d43d66e80f950382101
Vickyarts <65495793+Vickyarts@users.noreply.github.com>
2020-07-30 05:31:16 +0000

    Update Zone.sh

--- Zone.sh
+++ Zone.sh
@@ -4,6 +4,7 @@
 red='\e[1;31m'
 yellow='\e[1;33m'
 blue='\e[1;34m'
+white='\e[1;37m'
 
 if [ $# -eq 1 ];
 then
@@ -16,11 +17,11 @@ then
           echo -e $red "Zone Transfer Failed!"
         else
           echo -e $blue "Zone Transferm Success:"
-          echo "$zone"
+          echo -e $white "$zone"
         fi
      done
 else
-  echo -e $yellow"Usage:"
-  echo "Zone.sh <website>"
+  echo -e $yellow "Usage:"
+  echo -e $white "Zone.sh <website>"
   exit
 fi
